8e07853ec69eca42cfe2fd9af45fd790e668db90
Denis Lafontant <icebox827@gmail.com>
2022-08-28 08:52:29 -0400

    Update folder structure

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,11 @@
 import '../styles/App.css';
+import Home from '../containers/Home';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Home />
       </header>
     </div>
   );
