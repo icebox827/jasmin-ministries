258d11b7b922f2c7d4e84b7fcdd6bcf69b3e718b
Denis Lafontant <icebox827@gmail.com>
2022-08-28 17:26:36 -0400

    pdate app

---
+++ src/components/Routes.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import NavBar from './NavBar';
+import Home from '../containers/Home';
+import Footer from './Footer';
+
+function RoutesLink() {
+  return (
+    <Router>
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+      <Footer />
+    </Router>
+  );
+}
+
+export default RoutesLink;
---
+++ src/containers/Home.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Home() {
+  return (
+    <div className="hero">
+      <h1>Hello World!</h1>
+    </div>
+  );
+}
+
+export default Home;
