5f30841d4583b98f30ce4ddc73a262cb7506531f
Denis Lafontant <icebox827@gmail.com>
2022-08-28 11:44:48 -0400

    Add assets

---
+++ src/components/NavBar.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function NavBar() {
+	return (
+		hello
+	)
+}
+
+import default NavBar;
+
+Photo by Luis Quintero: https://www.pexels.com/photo/women-wearing-brown-dress-close-up-photography-2774546/
